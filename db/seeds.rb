056d02523a45d8ce5091a94fd7b06ff0b7c2f4ac
Christoph Matthies <chrisma@users.noreply.github.com>
2017-11-03 19:59:22 +0100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+FactoryBot.create(:player)
+FactoryBot.create(:player)
+FactoryBot.create(:match)
