f7e4f5900766c4ded30d7e50466e9ec87b9d4d7e
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-11-08 16:32:07 +0100

    Use errbit key from Env

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.host = 'https://swt2-errbit-2017.herokuapp.com'
   config.project_id = 1 # required, but any positive integer works
-  config.project_key = '06ad9d54ff00ce2d8a0afcbd5289ed12'
+  config.project_key = ENV['ERRBIT_API_KEY']
   config.environment = Rails.env
   config.ignore_environments = %w(development test)
 end
