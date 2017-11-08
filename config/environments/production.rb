b1ddc6254f7caff977ce3a8d59343e064bf9fa29
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-11-08 16:35:50 +0100

    Use airbrake in production ownly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,10 @@
+Airbrake.configure do |config|
+  config.host = 'https://swt2-errbit-2017.herokuapp.com'
+  config.project_id = 1 # required, but any positive integer works
+  config.project_key = ENV['ERRBIT_API_KEY']
+  config.environment = Rails.env
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
