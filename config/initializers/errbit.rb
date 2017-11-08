1e8237b04c21bdf075ceaf0e1155dbb285e1d851
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-11-08 16:01:44 +0100

    Change key

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.host = 'https://swt2-errbit-2017.herokuapp.com'
   config.project_id = 1 # required, but any positive integer works
-  config.project_key = '0cb97a3d4465c438daa8e8bd5471937b'
+  config.project_key = '06ad9d54ff00ce2d8a0afcbd5289ed12'
   config.environment = Rails.env
   config.ignore_environments = %w(development test)
 end
