9d3e8cb2d9e75c3d7977cb8dc527ad3c539b4240
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-11-08 16:37:58 +0100

    Remove test error

--- config/initializers/errbit.rb
+++
@@ -1,7 +0,0 @@
-Airbrake.configure do |config|
-  config.host = 'https://swt2-errbit-2017.herokuapp.com'
-  config.project_id = 1 # required, but any positive integer works
-  config.project_key = ENV['ERRBIT_API_KEY']
-  config.environment = Rails.env
-  config.ignore_environments = %w(development test)
-end
