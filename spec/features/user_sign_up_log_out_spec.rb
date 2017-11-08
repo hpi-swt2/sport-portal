92f4af451a7560a1ec46d444d9bd8d4dd7479c4f
Tobias Dürschmid <tobias.duerschmid@t-online.de>
2017-11-08 16:31:22 +0100

    Add test

---
+++ spec/features/user_sign_up_log_out_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "User management", :type => :feature do
+  scenario "User signs up" do
+    attrs = FactoryBot.attributes_for(:user)
+    visit new_user_registration_path
+
+    within('form', id: 'new_user') do
+      # fill_in can also locate input fields by their 'name' attribute
+      # http://www.rubydoc.info/github/jnicklas/capybara/Capybara/Node/Actions:fill_in
+      fill_in "user[first_name]", :with => attrs[:first_name]
+      fill_in "user[email]", :with => attrs[:email]
+      fill_in "user[password]", :with => attrs[:password]
+      fill_in "user[password_confirmation]", :with => attrs[:password_confirmation]
+      find('input[type="submit"]').click
+    end
+
+    expect(page).to have_text(I18n.t('devise.registrations.signed_up'))
+  end
+
+  scenario "User signs out from root page" do
+    sign_in FactoryBot.create(:user)
+    visit root_path
+    find("a[href='#{destroy_user_session_path}']").click
+
+    expect(page).to have_text(I18n.t('devise.sessions.signed_out'))
+  end
+end
