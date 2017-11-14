a6c635867c2dc12beccdd90a7b1b0a07d244c601
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-14 23:48:07 +0100

    added failing tests for #49

---
+++ spec/models/league_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "League model", type: :model do
+
+    it "should get created with correct data" do
+        league = League.new(name: "Weitwurfmeisterschaften",
+            description: "Throw! Throw! Record!",
+            discipline: "Kokosnussweitwurf",
+            player_type: Event.player_types[:single], max_teams: 8,
+            game_mode: League.game_modes[:round_robin])
+
+        expect(league.name).to eq("Weitwurfmeisterschaften")
+        expect(league.description).to eq("Throw! Throw! Record!")
+        expect(league.discipline).to eq("Throw! Throw! Record!")
+        expect(league.player_type).to eq(Event.player_types[:single])
+        expect(league.max_teams).to eq(8)
+        expect(league.game_mode).to eq(League.game_modes[:round_robin])
+    end
+
+end
---
+++ spec/models/tournament_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Tournament model", type: :model do
+
+    it "should get created with correct data" do
+        tournament = Tournament.new(name: "Weitwurfmeisterschaften",
+            description: "Throw! Throw! Record!",
+            discipline: "Kokosnussweitwurf",
+            player_type: Event.player_types[:single], max_teams: 8,
+            game_mode: Tournament.game_modes[:double_elimination])
+
+        expect(tournament.name).to eq("Weitwurfmeisterschaften")
+        expect(tournament.description).to eq("Throw! Throw! Record!")
+        expect(tournament.discipline).to eq("Throw! Throw! Record!")
+        expect(tournament.player_type).to eq(Event.player_types[:single])
+        expect(tournament.max_teams).to eq(8)
+        expect(tournament.game_mode).to eq(Tournament.game_modes[:double_elimination])
+    end
+
+end
