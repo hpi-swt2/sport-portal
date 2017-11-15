d4aed9d2598287540cf0d969ca38c2b4a88135c8
Jascha Beste <jascha.beste@student.hpi.de>
2017-11-15 10:50:18 +0100

    Generating schedule all against all. Event model impovements. Schedule display page, manual input for points. Ref #35

--- spec/routing/matches_routing_spec.rb
+++ spec/routing/matches_routing_spec.rb
@@ -3,10 +3,6 @@ require "rails_helper"
 RSpec.describe MatchesController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/matches").to route_to("matches#index")
-    end
-
     it "routes to #new" do
       expect(:get => "/matches/new").to route_to("matches#new")
     end
@@ -29,6 +25,14 @@ RSpec.describe MatchesController, type: :routing do
 
     it "routes to #update via PATCH" do
       expect(:patch => "/matches/1").to route_to("matches#update", :id => "1")
+    end
+
+    it "routes to #update_poinst via PUT" do
+      expect(:put => "/matches/1/update_points").to route_to("matches#update_points", :id => "1")
+    end
+
+    it "routes to #update_poinst via PATCH" do
+      expect(:put => "/matches/1/update_points").to route_to("matches#update_points", :id => "1")
     end
 
     it "routes to #destroy" do
