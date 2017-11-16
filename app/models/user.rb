eb412eebb62e034e5a2be41fa3d018ac3e9dd647
Niklas Riekenbrauck <nikriek@gmail.com>
2017-11-16 15:46:03 +0100

    Add avatar to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
 
   # Avatars
   has_attached_file :avatar, 
-    styles: { medium: "300x300>", thumb: "100x100>" }, 
+    styles: { medium: "300x300>", thumb: "100x100>", square: "100x100" }, 
     default_url: "/images/:style/missing.png"
   validates_attachment :avatar, 
     content_type: { content_type: /\Aimage\/.*\z/ }, 
