a32d07a06854fe02cc47717a5d872c0af3dbccc2
emanuel.metzenthin <emanuel.metzenthin@student.hpi.de>
2017-11-17 16:22:52 +0100

    Add basic abilities for default users and admins for #46

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -26,16 +26,15 @@ class Ability
   # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    if user.present?
+      can :manage, User, id: user.id
+      can :read, Event
+      can :create, Team
+      can :create, Match
 
-    user ||= User.new # guest user (not logged in)
-    # All users can only update their own user attributes
-    can :update, User, id: user.id
+      if user.admin?
+        can :manage, :all
+      end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116134818) do
+ActiveRecord::Schema.define(version: 20171117134817) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20171116134818) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
