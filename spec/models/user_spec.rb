64c0842c0740293b24fed91f0d382bced30654c5
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-18 17:50:38 +0100

    Extend user model specification to include #from_omniauth. Ref #59

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,8 +17,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "is valid when produced by a factory" do
+  it 'is valid when produced by a factory' do
     user = FactoryBot.build(:user)
     expect(user).to be_valid
+  end
+
+  describe '#from_omniauth' do
+    it 'should return an existing user' do
+      user = FactoryBot.create :user, provider: 'mock', uid: '1234567890'
+      autohash = OmniAuth::AuthHash.new(provider: 'mock', uid: '1234567890')
+      expect(User.from_omniauth(autohash).id).to eq(user.id)
+    end
+
+    it 'should create a new user when a new autohash is given' do
+      autohash = OmniAuth::AuthHash.new(provider: 'mock', uid: '1234567890')
+      user = User.from_omniauth autohash
+      expect(user.persisted?).to eq(false)
+    end
   end
 end
