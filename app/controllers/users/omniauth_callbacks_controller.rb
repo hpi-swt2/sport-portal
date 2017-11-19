725a0b747e75e24ca646ae37917a9a8ea11457b5
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-19 22:33:53 +0100

    Fix indentation.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,34 +13,34 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   protected
 
-  def attempt_sign_in_with(auth)
-    user = User.from_omniauth(auth)
-    if user
-      sign_in_user user
-    else
-      redirect_to root_path
-      set_flash_message(:error, :failure, provider: 'OpenID') if is_navigational_format?
+    def attempt_sign_in_with(auth)
+      user = User.from_omniauth(auth)
+      if user
+        sign_in_user user
+      else
+        redirect_to root_path
+        set_flash_message(:error, :failure, provider: 'OpenID') if is_navigational_format?
+      end
     end
-  end
 
-  def sign_in_user(user)
-    sign_in_and_redirect user, event: :authentication #will throw if user is not activated
-    set_flash_message(:notice, :success, provider: 'OpenID') if is_navigational_format?
-  end
-
-  def attempt_link_with(auth)
-    user = current_user
-    if user.has_omniauth?
-      set_flash_message(:error, :link_failure, provider: 'OpenID') if is_navigational_format?
-    else
-      link_with user, auth
+    def sign_in_user(user)
+      sign_in_and_redirect user, event: :authentication #will throw if user is not activated
+      set_flash_message(:notice, :success, provider: 'OpenID') if is_navigational_format?
     end
-    redirect_to user_path(user)
-  end
 
-  def link_with(user, auth)
-    user.omniauth = auth
-    user.save!
-    set_flash_message(:notice, :link_success, provider: 'OpenID') if is_navigational_format?
-  end
+    def attempt_link_with(auth)
+      user = current_user
+      if user.has_omniauth?
+        set_flash_message(:error, :link_failure, provider: 'OpenID') if is_navigational_format?
+      else
+        link_with user, auth
+      end
+      redirect_to user_path(user)
+    end
+
+    def link_with(user, auth)
+      user.omniauth = auth
+      user.save!
+      set_flash_message(:notice, :link_success, provider: 'OpenID') if is_navigational_format?
+    end
 end
