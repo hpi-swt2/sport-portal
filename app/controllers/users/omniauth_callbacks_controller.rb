65153dfce741c8eae6d4368f42e8cf056b86312f
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-19 20:43:12 +0100

    Add user linking via OpenID. Ref #59

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,12 +5,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def hpiopenid
     if user_signed_in?
       @user = current_user
-      #TODO: link account
+      if @user.provider.nil? and @user.uid.nil?
+        auth = request.env['omniauth.auth']
+        @user.uid = auth.uid
+        @user.provider = auth.provider
+        @user.save!
+        set_flash_message(:notice, :link_success, provider: 'OpenID') if is_navigational_format?
+      else
+        set_flash_message(:error, :link_failure, provider: 'OpenID') if is_navigational_format?
+      end
+      redirect_to user_path(@user)
     else
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user
         sign_in_and_redirect @user, event: :authentication #will throw if user is not activated
-        set_flash_message(:notice, :success_link, provider: 'OpenID') if is_navigational_format?
+        set_flash_message(:notice, :success, provider: :openid) if is_navigational_format?
       else
         redirect_to root_path
       end
