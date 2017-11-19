64c393f25de477f78ddbb4cba3bdba1b6d5d6589
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-19 16:07:59 +0100

    Add openid as an authentication provider. Ref #59

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -255,6 +255,14 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+  config.omniauth :openid,
+                  name: 'hpiopenid',
+                  identifier: 'https://openid.hpi.uni-potsdam.de/serve',
+                  required: [OmniAuth::Strategies::OpenID::AX[:email]],
+                  optional: []
+
+  require 'openid/fetchers'
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,16 @@ Rails.application.routes.draw do
   resources :matches
 
   # Use custom user controller instead of the one provided by devise
-  devise_for :users, :controllers => { registrations: 'users' }
+  devise_for :users, controllers: {
+    registrations: 'users',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   # Sets the devise scope to be used in the controller.
   # http://www.rubydoc.info/github/plataformatec/devise/ActionDispatch%2FRouting%2FMapper%3Adevise_scope
   devise_scope :user do
     resources :users, only: [:index, :show]
-    get '/users/:id/dashboard', to: "users#dashboard"
+    get '/users/:id/dashboard', to: 'users#dashboard'
   end
 
   #Define route for user dashboard
