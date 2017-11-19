a79858dc318ecaadf4fb18b3cf5f311464469027
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-19 17:29:03 +0100

    adapted tests to use factory

--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -1,20 +1,10 @@
 require 'rails_helper'
 
 describe "League model", type: :model do
-
-    it "should get created with correct data" do
-        league = League.new(name: "Weitwurfmeisterschaften",
-            description: "Throw! Throw! Record!",
-            discipline: "Kokosnussweitwurf",
-            player_type: Event.player_types[:single], max_teams: 8,
-            game_mode: League.game_modes[:round_robin])
-
-        expect(league.name).to eq("Weitwurfmeisterschaften")
-        expect(league.description).to eq("Throw! Throw! Record!")
-        expect(league.discipline).to eq("Kokosnussweitwurf")
-        expect(league.player_type).to eq(Event.player_types[:single])
-        expect(league.max_teams).to eq(8)
-        expect(league.game_mode).to eq(League.game_modes[:round_robin])
+    
+    it "is valid when produced by a factory" do
+      league = FactoryBot.build(:league)
+      expect(league).to be_valid
     end
 
 end
--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -2,19 +2,9 @@ require 'rails_helper'
 
 describe "Tournament model", type: :model do
 
-    it "should get created with correct data" do
-        tournament = Tournament.new(name: "Weitwurfmeisterschaften",
-            description: "Throw! Throw! Record!",
-            discipline: "Kokosnussweitwurf",
-            player_type: Event.player_types[:single], max_teams: 8,
-            game_mode: Tournament.game_modes[:double_elimination])
-
-        expect(tournament.name).to eq("Weitwurfmeisterschaften")
-        expect(tournament.description).to eq("Throw! Throw! Record!")
-        expect(tournament.discipline).to eq("Kokosnussweitwurf")
-        expect(tournament.player_type).to eq(Event.player_types[:single])
-        expect(tournament.max_teams).to eq(8)
-        expect(tournament.game_mode).to eq(Tournament.game_modes[:double_elimination])
+    it "is valid when produced by a factory" do
+      league = FactoryBot.build(:league)
+      expect(league).to be_valid
     end
 
 end
