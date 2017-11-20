6f64e1050b23e35de69ba47277c4645f81ae3257
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-20 11:37:18 +0100

    changed usage of factory in test to accomodate testing the constructor

--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe "League model", type: :model do
-    
+
     it "is valid when produced by a factory" do
-      league = FactoryBot.build(:league)
+      league = FactoryBot.build(:league, name: nil)
       expect(league).to be_valid
     end
 
