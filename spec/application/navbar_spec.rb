3c6e792ffe94fa66013510dfc3afeaf6fbac2ade
Moritz Manner <moritz.manner@gmail.com>
2017-11-20 19:35:38 +0100

    navbar specs for dropdown menu. ref #37

---
+++ spec/application/navbar_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature "Page has navbar", :type => :feature do
+  context 'when the user is signed in' do
+    before(:each) do
+      user = FactoryBot.create(:user)
+      sign_in user
+    end
+
+    it "has a dropdown menu" do
+      visit root_path
+      expect(page).to have_css(".navbar .dropdown")
+    end
+
+    it "dropdown menu shows menu button" do
+      visit root_path
+      expect(page).to have_css(".navbar .dropdown-toggle", :text => "Menu")
+    end
+
+    it "opens dropdown when hovered over" do
+      visit root_path
+      find(".dropdown").hover
+      #page.driver.browser.action.move_to(page.find('.dropdown').native).perform
+      expect(page).to have_css(".navbar .dropdown-menu", :text => "Sign out")
+    end
+  end
+
+  context 'when the user logged out' do
+    it "doesn't have a dropdown menu" do
+      visit root_path
+      expect(page).not_to have_css(".navbar .dropdown")
+    end
+
+    it "has a login button" do
+      visit root_path
+      expect(page).to have_css(".navbar", :text => "Sign in")
+    end
+  end
+end
