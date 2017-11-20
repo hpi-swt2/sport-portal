c7f128f1cb33623040571d5cfe3d1812fb45ada4
Tobias Bredow <tobias.bredow@student.hpi.de>
2017-11-20 18:51:58 +0100

    cosmetic changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -54,6 +54,6 @@ class EventsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def event_params
-      params.require(:event).permit(:name, :description, :gamemode, :sport, :teamsport, :playercount, :gamesystem, :deadline, )
+      params.require(:event).permit(:name, :description, :gamemode, :sport, :teamsport, :playercount, :gamesystem, :deadline)
     end
 end
