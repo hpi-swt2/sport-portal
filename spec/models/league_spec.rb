3c3de6d2afd9798ee44c34b9963ef405ce4d2a55
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-20 11:40:02 +0100

    fixed wrong testing

--- spec/models/league_spec.rb
+++ spec/models/league_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "League model", type: :model do
 
     it "is valid when produced by a factory" do
-      league = FactoryBot.build(:league, name: nil)
+      league = FactoryBot.build(:league)
       expect(league).to be_valid
     end
 
