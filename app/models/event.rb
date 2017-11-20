7e88f8cf468ad50da20906865fae2580dd220d41
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-20 11:29:48 +0100

    added schema information to event class

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,21 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  name                         :string
+#  description                  :text
+#  discipline                   :string
+#  player_type                  :integer
+#  max_teams                    :integer
+#  game_mode                    :integer         not null
+#  type                         :integer
+#  created_at                   :datetime        not null
+#  updated_at                   :datetime        not null
+#  index_events_on_game_mode    :index ["game_mode"]
+#  index_events_on_player_type  :index ["player_type"]
+#
+
 class Event < ApplicationRecord
-  #Represents a sports event, to be subclassed by league or tournament
-  
   enum player_types: [:single, :team]
 
   def self.types
