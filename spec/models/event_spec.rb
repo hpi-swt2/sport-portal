fee26876f292dd4d4dd864f22ae8825e50c79a12
Laurenz Seidel <LaurenzSeidel@gmail.com>
2017-11-20 11:42:08 +0100

    removed duplication from test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,21 +4,18 @@ describe "Event model", type: :model do
 
     it "should not validate without name" do
         league = FactoryBot.build(:league, name: nil)
-        league.name = nil
 
         expect(league.valid?).to eq(false)
     end
 
     it "should not validate without discipline" do
         league = FactoryBot.build(:league, discipline: nil)
-        league.discipline = nil
 
         expect(league.valid?).to eq(false)
     end
 
     it "should not validate without game_mode" do
         league = FactoryBot.build(:league, game_mode: nil)
-        league.game_mode = nil
 
         expect(league.valid?).to eq(false)
     end
