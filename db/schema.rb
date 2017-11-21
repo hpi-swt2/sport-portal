7ec5fa7a66d0a7e411761493ece10d1103697e9e
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-21 16:51:05 +0100

    Add unique index over omniauth to users. Ref #59

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119145250) do
+ActiveRecord::Schema.define(version: 20171121154832) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171119145250) do
     t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
