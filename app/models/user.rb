3eb14667ec1b725b2a4c076884c1a30e23d8f000
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-21 13:43:52 +0100

    Update user model schema comments. Ref #59

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@
 #  updated_at             :datetime         not null
 #  first_name             :string
 #  last_name              :string
+#  provider               :string
+#  uid                    :string
 #
 
 class User < ApplicationRecord
