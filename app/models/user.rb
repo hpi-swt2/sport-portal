8919db208882fa341b5c4aa29aae3993c7807ed5
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-11-21 17:11:26 +0100

    Add uniqueness validation to user omniauth. Ref #59

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:hpiopenid]
 
   validates :first_name, presence: true
+  validates :uid, uniqueness: { scope: :provider, allow_nil: true }
 
   def has_omniauth?
     provider.present? && uid.present?
