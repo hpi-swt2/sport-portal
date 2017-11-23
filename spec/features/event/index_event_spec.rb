eca48368494fc4cd88db36b0d0d9bc1b6910f9ec
Jeilef <justus.eilers@student.hpi.de>
2017-11-23 17:54:12 +0100

    upgraded tests for signing in user

--- spec/features/event/index_event_spec.rb
+++ spec/features/event/index_event_spec.rb
@@ -5,22 +5,19 @@ require'rails_helper'
       @teamevent = FactoryBot.create :event
       @teamevent.teamsport = true
 
-      @user = FactoryBot.create(:user, password: '123456', password_confirmation: '123456')
-      visit new_user_session_path
-      within('form', id: 'new_user') do
-        fill_in "user[email]", :with => @user[:email]
-        fill_in "user[password]", :with => '123456'
-        find('input[type="submit"]').click
-      end
+      @user = FactoryBot.create(:user)
+
     end
 
     it "should not be possible to join team event via join button" do
+      sign_in @user
       visit events_path
 
       expect(page).not_to have_button("Join Event")
     end
 
     it "should not be possible to join team event via join button" do
+      sign_in @user
       @singleevent = FactoryBot.create :event
       visit events_path
 
@@ -28,6 +25,7 @@ require'rails_helper'
     end
 
     it "should be possible that label of join button changes to 'Leave Event' when user joins" do
+      sign_in @user
       @singleevent = FactoryBot.create :event
       visit events_path
       click_button("Join Event")
@@ -36,6 +34,7 @@ require'rails_helper'
     end
 
     it "should be possible for a user to see, which event he/she joined" do
+      sign_in @user
       @singleevent = FactoryBot.create :event
       visit events_path
       click_button("Join Event")
@@ -44,6 +43,8 @@ require'rails_helper'
     end
 
     it "should be possible to see join button only when deadline has not been passed" do
+
+      sign_in @user
       @singleevent = FactoryBot.create :event
       @singleevent.deadline = Date.yesterday
       visit events_path
@@ -52,7 +53,6 @@ require'rails_helper'
     end
 
     it "should be not possible to join an event if the user is not logged in" do
-      @user = FactoryBot.create :user
       @singleevent = FactoryBot.create :event
 
       visit events_path()
--- spec/features/event/show_event_spec.rb
+++ spec/features/event/show_event_spec.rb
@@ -2,13 +2,7 @@ require'rails_helper'
 
 describe "detailled event page", type: :feature do
   before(:each) do
-    @user = FactoryBot.create(:user, password: '123456', password_confirmation: '123456')
-    visit new_user_session_path
-    within('form', id: 'new_user') do
-      fill_in "user[email]", :with => @user[:email]
-      fill_in "user[password]", :with => '123456'
-      find('input[type="submit"]').click
-    end
+    @user = FactoryBot.create(:user)
 
     @teamevent = FactoryBot.create :event
     @teamevent.teamsport = true
@@ -16,19 +10,20 @@ describe "detailled event page", type: :feature do
   end
 
   it "should not be possible to join team event via join button" do
+    sign_in @user
     visit events_path(@teamevent)
-
     expect(page).not_to have_button("Join Event")
   end
 
   it "should not be possible to join team event via join button" do
-
+    sign_in @user
     visit events_path(@singleevent)
 
     expect(page).to have_button("Join Event")
   end
 
   it "should be possible that label of join button changes to 'Leave Event' when user joins" do
+    sign_in @user
     visit events_path(@singleevent)
     click_button("Join Event")
 
@@ -36,6 +31,7 @@ describe "detailled event page", type: :feature do
   end
 
   it "should be possible for a user to see, which event he/she joined" do
+    sign_in @user
     visit events_path(@singleevent)
     click_button("Join Event")
 
@@ -43,7 +39,6 @@ describe "detailled event page", type: :feature do
   end
 
   it "should be not possible to join an event if the user is not logged in" do
-    @user = FactoryBot.create :user
     visit events_path(@singleevent)
     expect(page).not_to have_button("Join Event")
   end
