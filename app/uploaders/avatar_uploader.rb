cce83fb1e1b2f3783ab9de5bd2622bd3f9ea68a1
Niklas Riekenbrauck <nikriek@gmail.com>
2017-11-23 15:34:56 +0100

    Update intendation

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,6 +5,6 @@ class AvatarUploader < Shrine
 
   Attacher.validate do
     validate_mime_type_inclusion %w[image/jpeg image/gif image/png]
-	  validate_max_size 2.megabytes, message: 'is too large (max is 2 MB)'
+    validate_max_size 2.megabytes, message: 'is too large (max is 2 MB)'
   end
 end
