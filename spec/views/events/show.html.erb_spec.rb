6886ca14679c30946c414092f5d3ab84a9213a94
deeps96 <leonardo.huebscher.business@gmail.com>
2017-11-24 11:07:23 +0100

    Add failing tests for event view show. #46

--- spec/views/events/show.html.erb_spec.rb
+++ spec/views/events/show.html.erb_spec.rb
@@ -30,4 +30,51 @@ RSpec.describe "events/show", type: :view do
     expect(rendered).to have_css('a.btn.btn-default', :count => 2)
     expect(rendered).to have_css('a.btn.btn-danger')
   end
+
+  #not signed in user
+  it "doesn't render the new button when not signed in" do
+    render
+    expect(rendered).to_not have_selector(:link_or_button, 'New')
+  end
+
+  it "doesn't render the edit button when not signed in" do
+    render
+    expect(rendered).to_not have_selector(:link_or_button, 'Edit')
+  end
+
+  it "doesn't render the delete button when not signed in" do
+    render
+    expect(rendered).to_not have_selector(:link_or_button, 'Delete')
+  end
+
+  #signed in user
+  it "render the new button when signed in" do
+    render
+    expect(rendered).to have_selector(:link_or_button, 'New')
+  end
+
+  it "doesn't render the edit button when the event doesn´t belong to the user" do
+    sign_in @user
+    render
+    expect(rendered).to_not have_selector(:link_or_button, 'Edit')
+  end
+
+  it "doesn't render the delete button when the event doesn´t belong to the user" do
+    sign_in @user
+    expect(rendered).to_not have_selector(:link_or_button, 'Delete')
+  end
+
+  it "renders the edit button when the event belongs to the user" do
+    sign_in @user
+    @events[1].creator = @user
+    render
+    expect(rendered).to have_selector(:link_or_button, 'Edit')
+  end
+
+  it "renders the delete button when signed in and it's your team" do
+    sign_in @user
+    @events[1].creator = @user
+    render
+    expect(rendered).to have_selector(:link_or_button, 'Delete')
+  end
 end
