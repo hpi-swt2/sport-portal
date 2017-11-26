abac8457f1ed4ebe170d26200c2bd40bfa62948a
Usame Algan <usame.algan@student.hpi.uni-potsdam.de>
2017-11-26 14:37:19 +0100

    removed double validation from subclasses. Ref:#19

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,5 @@
 class League < Event
   enum game_modes: [:round_robin, :two_halfs, :swiss, :danish]
 
-  validates :name, :discipline, :game_mode, presence: true
+  # validates :name, :discipline, :game_mode, presence: true
 end
