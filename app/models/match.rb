d44a36aca7b917801c01d68f12f892aced9ffdb4
Marius Danner <marius.danner@student.hpi.de>
2017-11-29 15:48:47 +0100

    Add cascade deletion of matches in case the event gets deleted. Ref #46

--- app/models/match.rb
+++ app/models/match.rb
@@ -16,5 +16,5 @@
 class Match < ApplicationRecord
   belongs_to :team_home, class_name: 'Team', foreign_key: 'team_home_id'
   belongs_to :team_away, class_name: 'Team', foreign_key: 'team_away_id'
-  belongs_to :event
+  belongs_to :event, dependent: :delete_all
 end
