f9ad5e2e9e3492e56b02956f5cd075cd8d4cbb81
bakoe <ba.koe96@gmail.com>
2017-11-30 15:01:51 +0100

    Extract methods in ability.rb to avoid codeclimate issues. Ref #24

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -40,32 +40,50 @@ class Ability
     # All users can only update their own user attributes
     can :update, User, id: user_id
 
-    can :assign_ownership, Team, Team do |team|
-      team.owners.include? user
-    end
+    can_assign_ownership(user)
 
-    can :delete_ownership, Team, Team do |team|
-      team_owners = team.owners
-      team_owners.include? user
-      team_owners.length > 1
-    end
+    can_delete_ownership(user)
 
-    can :delete_membership, Team, Team do |team|
-      exist_owners_after_delete = owners_after_delete = Ability.number_of_owners_after_delete(team, team_member) > 0
-      ((team.owners.include? user) && exist_owners_after_delete) || ((user_id == Integer(team_member)) && exist_owners_after_delete)
-    end
+    can_delete_membership(team_member, user)
 
     can :read, Team, private: false
 
     if user.present?
-      can :create, Team
-      can :read, Team, private: true, members: { id: user_id }
-      can :update, Team, members: { id: user_id }
-      can :destroy, Team, owners: { id: user_id }
+      can_crud_team(user)
     end
   end
 
   private
+    def can_crud_team(user)
+      user_id = user.id
+      can :create, Team
+      can :read, Team, private: true, members: {id: user_id}
+      can :update, Team, members: {id: user_id}
+      can :destroy, Team, owners: {id: user_id}
+    end
+
+    def can_assign_ownership(user)
+      can :assign_ownership, Team, Team do |team|
+        team.owners.include? user
+      end
+    end
+
+    def can_delete_membership(team_member, user)
+      can :delete_membership, Team, Team do |team|
+        user_id = user.id
+        exist_owners_after_delete = owners_after_delete = Ability.number_of_owners_after_delete(team, team_member) > 0
+        ((team.owners.include? user) && exist_owners_after_delete) || ((user_id == Integer(team_member)) && exist_owners_after_delete)
+      end
+    end
+
+    def can_delete_ownership(user)
+      can :delete_ownership, Team, Team do |team|
+        team_owners = team.owners
+        team_owners.include? user
+        team_owners.length > 1
+      end
+    end
+
     def self.number_of_owners_after_delete(team, team_member)
       owners = team.owners
       another_user = User.find(team_member)
