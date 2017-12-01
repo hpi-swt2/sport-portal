e979be5be0af6454bce4605bd79aa63e705c2012
bakoe <ba.koe96@gmail.com>
2017-12-01 14:33:11 +0100

    Remove obsolete TeamOwner and TeamMember models. Ref #190

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130200433) do
+ActiveRecord::Schema.define(version: 20171201132608) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -66,18 +66,6 @@ ActiveRecord::Schema.define(version: 20171130200433) do
     t.integer "event_id"
     t.index ["event_id"], name: "index_organizers_on_event_id"
     t.index ["user_id"], name: "index_organizers_on_user_id"
-  end
-
-  create_table "team_members", id: false, force: :cascade do |t|
-    t.integer "team_id", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_team_members_on_user_id_and_user_id"
-  end
-
-  create_table "team_owners", id: false, force: :cascade do |t|
-    t.integer "team_id", null: false
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_team_owners_on_user_id_and_user_id"
   end
 
   create_table "team_users", id: false, force: :cascade do |t|
