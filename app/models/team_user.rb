131265841cd31e8e11eb43bb0767011911906533
bakoe <ba.koe96@gmail.com>
2017-12-01 16:08:39 +0100

    Remove unnecessary `is_owner?` method from TeamUser model. Ref #190

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -4,11 +4,7 @@ class TeamUser < ApplicationRecord
   belongs_to :team
   belongs_to :user
   after_initialize :init
-
-  def is_owner?
-    is_owner
-  end
-
+  
   def init
     self.is_owner ||= false # New team members are by default no team owners
   end
