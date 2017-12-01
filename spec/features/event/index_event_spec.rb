e954e32a1ad4a708e65d51e9a6db0da5cd15eaee
stlutz <stephan.lutz@student.hpi.de>
2017-12-01 11:16:25 +0100

    Add localization to join and leave event buttons.

--- spec/features/event/index_event_spec.rb
+++ spec/features/event/index_event_spec.rb
@@ -10,7 +10,7 @@ require'rails_helper'
       sign_in @user
       visit events_path
 
-      expect(page).not_to have_css('a#joinbutton.btn')
+      expect(page).not_to have_css('a#join_event_button.btn')
     end
 
     it "should display a button for single player events" do
@@ -18,22 +18,22 @@ require'rails_helper'
       @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
       visit events_path
 
-      expect(page).to have_css('a#joinbutton.btn')
+      expect(page).to have_css('a#join_event_button.btn')
     end
 
     it "should be possible that label of join button changes to 'Leave Event' when user joins" do
       sign_in @user
       @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
       visit events_path
-      click_link("Join Event")
-      expect(page).to have_css('a#leavebutton.btn')
+      click_link(:join_event_button)
+      expect(page).to have_css('a#leave_event_button.btn')
     end
 
     it "should be possible for a user to see, which event he/she joined" do
       sign_in @user
       @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
       visit events_path
-      click_link_or_button("Join Event")
+      click_link_or_button(:join_event_button)
 
       expect(page).to have_content("Participating")
     end
@@ -45,14 +45,14 @@ require'rails_helper'
       @singleevent.deadline = Date.yesterday
       visit events_path
 
-      expect(page).not_to have_button("Join Event")
+      expect(page).not_to have_button(:join_event_button)
     end
 
     it "should be not possible to join an event if the user is not logged in" do
       @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
 
       visit events_path()
-      expect(page).not_to have_button("Join Event")
+      expect(page).not_to have_button(:join_event_button)
     end
 
     it "should be possible to see all events if corresp. checkbox is enabled" do
--- spec/features/event/show_event_spec.rb
+++ spec/features/event/show_event_spec.rb
@@ -11,34 +11,34 @@ describe "detailled event page", type: :feature do
   it "should not display join button for team events" do
     sign_in @user
     visit event_path(@teamevent)
-    expect(page).not_to have_css('a#joinbutton.btn')
+    expect(page).not_to have_css('a#join_event_button.btn')
   end
 
   it "should be possible to join team event via join button" do
     sign_in @user
     visit event_path(@singleevent)
 
-    expect(page).to have_css('a#joinbutton.btn')
+    expect(page).to have_css('a#join_event_button.btn')
   end
 
   it "should be possible that label of join button changes to 'Leave Event' when user joins" do
     sign_in @user
     visit event_path(@singleevent)
-    click_link_or_button("Join Event")
+    click_link_or_button(:join_event_button)
 
-    expect(page).to have_css('a#leavebutton.btn')
+    expect(page).to have_css('a#leave_event_button.btn')
   end
 
   it "should be possible for a user to see, which event he/she joined" do
     sign_in @user
     visit event_path(@singleevent)
-    click_link("Join Event")
+    click_link_or_button(:join_event_button)
 
     expect(page).to have_content("Participating")
   end
 
   it "should be not possible to join an event if the user is not logged in" do
     visit event_path(@singleevent)
-    expect(page).not_to have_css('a#joinbutton.btn')
+    expect(page).not_to have_css('a#join_event_button.btn')
   end
 end
