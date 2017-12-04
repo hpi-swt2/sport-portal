5b1fb8a57ce91548e09cdb2646523bc6246c9a6a
stlutz <stephan.lutz@student.hpi.de>
2017-12-04 12:14:30 +0100

    Refactor tests for event index page.

--- spec/features/event/index_event_spec.rb
+++ spec/features/event/index_event_spec.rb
@@ -2,68 +2,79 @@ require 'rails_helper'
 
 describe "index event page", type: :feature do
   before(:each) do
-    @teamevent = FactoryBot.create(:event, player_type: Event.player_types[:team])
     @user = FactoryBot.create(:user)
   end
 
-  it "should not display a join button for teamevents" do
-    sign_in @user
-    visit events_path
-    expect(page).not_to have_css('a#join_event_button.btn')
+  context "for all event types" do
+    before(:each) do
+      @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
+      @teamevent = FactoryBot.create(:event, player_type: Event.player_types[:team])
+    end
+
+    it "should change the join button to a leave button after the user joins the event" do
+      sign_in @user
+      visit events_path
+      click_link(:join_event_button)
+      expect(page).not_to have_link(:join_event_button)
+      expect(page).to have_link(:leave_event_button)
+    end
+
+    it "should show the user whether they joined" do
+      sign_in @user
+      visit events_path
+      click_link_or_button(:join_event_button)
+      expect(page).to have_content("Participating")
+    end
+
+    it "should not allow the user to join if they are not logged in" do
+      visit events_path()
+      expect(page).not_to have_button(:join_event_button)
+    end
   end
 
+  context "for single player events" do
+    before(:each) do
+      @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
+    end
 
-  it "should display a button for single player events" do
-    sign_in @user
-    @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
-    visit events_path
-    expect(page).to have_css('a#join_event_button.btn')
+    it "should have a join button" do
+      sign_in @user
+      visit events_path
+      expect(page).to have_link(:join_event_button)
+    end
   end
 
-  it "should be possible that label of join button changes to 'Leave Event' when user joins" do
-     sign_in @user
-     @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
-     visit events_path
-     click_link(:join_event_button)
-     expect(page).to have_css('a#leave_event_button.btn')
+  context "for team events" do
+    before(:each) do
+      @teamevent = FactoryBot.create(:event, player_type: Event.player_types[:team])
+    end
+
+    it "should not display a join button" do
+      sign_in @user
+      visit events_path
+      expect(page).not_to have_link(:join_event_button)
+    end
   end
 
-  it "should be possible for a user to see, which event he/she joined" do
-     sign_in @user
-     @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
-     visit events_path
-     click_link_or_button(:join_event_button)
-     expect(page).to have_content("Participating")
-  end
+  context "for events whose deadline has passed" do
+    before(:each) do
+      @oldevent = FactoryBot.create :event, deadline: Date.yesterday
+    end
 
-  it "should be possible to see join button only when deadline has not been passed" do
+    it "should show them if the corresponding checkbox is enabled" do
+      visit "/events?showAll=on"
+      expect(page).to have_content(@oldevent.deadline.to_s)
+    end
 
-     sign_in @user
-     @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
-     @singleevent.deadline = Date.yesterday
-     visit events_path
-     expect(page).not_to have_button(:join_event_button)
-  end
+    it "should hide them if the corresponding checkbox is disabled" do
+      visit events_path
+      expect(page).not_to have_content(@oldevent.deadline.to_s)
+    end
 
-
-  it "should be not possible to join an event if the user is not logged in" do
-     @singleevent = FactoryBot.create :event, player_type: Event.player_types[:single]
-     visit events_path()
-     expect(page).not_to have_button(:join_event_button)
-  end
-
-
-  it "should be possible to see all events if corresp. checkbox is enabled" do
-    @oldevent = FactoryBot.create :event, deadline: Date.yesterday
-
-    visit "/events?showAll=on"
-
-    expect(page).to have_content(@oldevent.deadline.to_s)
-  end
-
-  it "should be possible to see active events only if corresp. checkbox is disabled" do
-    @oldevent = FactoryBot.create :event, deadline: Date.yesterday
-    visit "/events?"
-    expect(page).not_to have_content(@oldevent.deadline.to_s)
+    it "should not display a join button" do
+      sign_in @user
+      visit "/events?showAll=on"
+      expect(page).not_to have_button(:join_event_button)
+    end
   end
 end
