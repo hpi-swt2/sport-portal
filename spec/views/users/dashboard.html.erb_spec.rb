6b91e9cd2d51b7aa416b848003297c2a15f9cb29
jak-ing <lupin2702@gmail.com>
2017-12-05 14:47:24 +0100

    Add model, view and feature tests for dashboard access control #45

---
+++ spec/views/users/dashboard.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "devise/registrations/dashboard", type: :view do
+  before(:each) do
+    @user = assign(:user, FactoryBot.create(:user))
+    sign_in @user
+  end
+
+  it "renders headline" do
+    render
+    expect(rendered).to have_css('h1', text: I18n.t('dashboard.title'))
+  end
+end
