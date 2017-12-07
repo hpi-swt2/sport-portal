4fad067a696cca0a4e2fd88c2f95892be05187e6
Niklas Riekenbrauck <nikriek@gmail.com>
2017-12-07 14:36:16 +0100

    Fix placeholder

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,13 +1,14 @@
 class EventMailer < ApplicationMailer
   # Notifies a user about an upcoming match
-  def event_notification(user, event)
-      @user = user
-      @event = event
-      locale = Rails.application.config.default_locale
-      I18n.with_locale(locale) do
-        mail(to: @user.email,
-             subject: I18n.t('event_mailer.event_notification.subject', startdate: @event.startdate)
-        )
-      end
+  def event_notification(user, event, participants)
+    @user = user
+    @event = event
+    @participants = participants
+    locale = I18n.default_locale
+    I18n.with_locale(locale) do
+      mail(to: @user.email,
+           subject: I18n.t('event_mailer.event_notification.subject', startdate: @event.startdate)
+      )
+    end
   end
 end
