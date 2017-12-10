c0f2d01f29ef25a48bb55fffd824f45db23d35d6
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2017-12-10 13:45:09 +0100

    Update event form specs. Ref #104

--- spec/features/event/new_event_spec.rb
+++ spec/features/event/new_event_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "new event page", type: :feature do
+describe 'new event page', type: :feature do
   before(:each) do
     @user = FactoryBot.create :user
     sign_in @user
@@ -11,11 +11,11 @@ describe "new event page", type: :feature do
     @user.destroy
   end
 
-  it "should render without an error" do
+  it 'should render without an error' do
     visit new_event_path
   end
 
-  it "should be possible to enter date conditions for an event" do
+  it 'should be possible to enter date conditions for an event' do
     visit new_event_path
 
     expect(page).to have_field('event_deadline')
@@ -23,30 +23,23 @@ describe "new event page", type: :feature do
     expect(page).to have_field('event_enddate')
   end
 
-  it "should have a field for event duration" do
+  it 'should have a field for event duration' do
     visit new_event_path
 
     expect(page).to have_field('event_duration')
   end
 
-  it "should have a dropdown menu for type" do
-    visit new_event_path
+  it 'should be possible to create a date conditions for an event' do
+    visit new_tournament_path
+    fill_in 'event_name', with: 'name'
+    fill_in 'event_discipline', with: 'soccer'
+    select 'Ko', from: Event.human_attribute_name(:game_mode)
+    select 'Single', from: Event.human_attribute_name(:player_type)
+    fill_in 'event_max_teams', with: '5'
 
-    expect(page).to have_select('event_type', options: [I18n.t('events.new.select_type'), 'Tournament', 'League'])
-  end
-
-  it "should be possible to create a date conditions for an event" do
-    visit new_event_path
-    fill_in "event_name", with: "name"
-    fill_in "event_discipline", with: "soccer"
-    select "Tournament", from: "event_type"
-    select "Ko", from: "event_game_mode_tournament"
-    select "Single", from: "event_player_type"
-    fill_in "event_max_teams", with: "5"
-
-    fill_in "event_deadline", with: Date.tomorrow.to_s
-    fill_in "event_startdate", with: (Date.tomorrow + 2).to_s
-    fill_in "event_enddate", with: (Date.tomorrow + 3).to_s
+    fill_in 'event_deadline', with: Date.tomorrow.to_s
+    fill_in 'event_startdate', with: (Date.tomorrow + 2).to_s
+    fill_in 'event_enddate', with: (Date.tomorrow + 3).to_s
 
     find('input[type="submit"]').click
 
--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -1,36 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe "events/new", type: :view do
+RSpec.describe 'events/new', type: :view do
   before(:each) do
     assign(:event, FactoryBot.build(:event))
   end
 
-  it "should render the events form" do
+  it 'should render the events form' do
     render
     expect(rendered).to have_css("form[action='#{events_path}'][method='post']", count: 1)
   end
 
-  it "has input for all attributes" do
+  it 'has input for all attributes' do
     render
 
-    assert_select "form[action=?][method=?]", events_path, "post" do
-
-      assert_select "input[name=?]", "event[name]"
-
-      assert_select "textarea[name=?]", "event[description]"
-
-      assert_select "select[name=?]", "event[type]"
-
-      assert_select "select[name=?]", "event[game_mode]"
-
-      assert_select "input[name=?]", "event[discipline]"
-
-      assert_select "input[name=?]", "event[deadline]"
-
-      assert_select "input[name=?]", "event[startdate]"
-
-      assert_select "input[name=?]", "event[enddate]"
-
+    assert_select 'form[action=?][method=?]', events_path, "post" do
+      assert_select 'input[name=?]', 'event[name]'
+      assert_select 'textarea[name=?]', 'event[description]'
+      assert_select 'input[name=?]', 'event[discipline]'
+      assert_select 'input[name=?]', 'event[deadline]'
+      assert_select 'input[name=?]', 'event[startdate]'
+      assert_select 'input[name=?]', 'event[enddate]'
     end
   end
 
