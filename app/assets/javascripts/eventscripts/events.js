55f0ff3ab1b08cde0ac80c10d747665802b62ffe
Unknown <constantin.lange@student.hpi.de>
2017-12-10 16:52:00 +0100

    Fixed identation. Ref.: #52.

--- app/assets/javascripts/eventscripts/events.js
+++ app/assets/javascripts/eventscripts/events.js
@@ -1,24 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $( document ).on('turbolinks:load', function() {
-      var location = window.location.pathname;
-      var param = window.location.search.slice(1);
-      var checkbox = document.querySelector('#shown_events');
-      var state = localStorage.getItem('checked');
+  var location = window.location.pathname;
+  var param = window.location.search.slice(1);
+  var checkbox = document.querySelector('#shown_events');
+  var state = localStorage.getItem('checked');
 
-      checkbox.addEventListener('click', function() {
-        localStorage.setItem('checked',checkbox.checked);
-        $("form").submit();
-      });
-
-      if(state == 'true')
-      {
-        checkbox.checked = true;
-
-        if(location == "/events" && param != "showAll=on")
-        {
-          checkbox.checked = false;
-          localStorage.setItem('checked',checkbox.checked);
-        }
-      }
+  checkbox.addEventListener('click', function() {
+    localStorage.setItem('checked',checkbox.checked);
+    $("form").submit();
   });
+
+  if(state == 'true')
+  {
+    checkbox.checked = true;
+
+    if(location == "/events" && param != "showAll=on")
+    {
+      checkbox.checked = false;
+      localStorage.setItem('checked',checkbox.checked);
+    }
+  }
+});
