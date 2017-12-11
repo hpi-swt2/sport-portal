52bcbd75507bb119485400f83d87df170264064d
Adrian <a.kuchinke@gmail.com>
2017-12-11 11:51:18 +0100

    dummy form advanced event options #197

--- db/schema.rb
+++ db/schema.rb
@@ -98,14 +98,14 @@ ActiveRecord::Schema.define(version: 20171206172721) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "provider"
+    t.string "uid"
+    t.text "image_data"
     t.boolean "admin", default: false
     t.date "birthday"
     t.string "telephone_number"
     t.string "telegram_username"
     t.string "favourite_sports"
-    t.string "provider"
-    t.string "uid"
-    t.text "avatar_data"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
