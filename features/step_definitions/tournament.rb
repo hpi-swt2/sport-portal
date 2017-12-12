741498fcf258ed33c125edd32eabc9cbb61f81a5
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2017-12-12 12:25:46 +0100

    Added failing cucumber tets for spieplan

--- features/step_definitions/tournament.rb
+++ features/step_definitions/tournament.rb
@@ -15,3 +15,27 @@ end
 When(/^the tournament overview page for (.*) is visited$/) do |tournamentName|
   visit event_overview_path (tournament_named tournamentName)
 end
+
+
+When(/^the event page for (.*) is visited$/) do |tournamentName|
+  visit event_path (tournament_named tournamentName)
+end
+
+And(/^it should link to tournament Spielplan for (.*)$/) do |tournamentName|
+  click_button 'zum Spielplan'
+  expect(page).to have_current_path(event_schedule_path(tournament_named tournamentName), only_path: true)
+end
+
+When(/^the Spielplan page for (.*) is visited$/) do |tournamentName|
+  visit event_schedule_path(tournament_named tournamentName)
+end
+
+Then(/^there should be several rounds$/) do
+  expect(page).to have_content('Finale')
+  expect(page).to have_content('Halbfinale')
+end
+
+And(/^there should be exactly (\d+) matches and (\d+) rounds$/) do |numberOfMatches, numberOfRounds|
+  # The minus 1 is needed because the headline of the table is also a tr entry
+  (page.all('table#matches-table tr').count - 1).should == (numberOfMatches + numberOfRounds)
+end
---
+++ features/tournament/spielplan.feature
@@ -0,0 +1,17 @@
+Feature: Spielplan for tournament
+  As a tournament organizer
+  in order to organise a torunament
+  I want to have a complete Spielplan for my ko-tournament event
+
+  Background:
+    Given a tournament t with 16 teams
+
+  Scenario: Spielplan should be linked from tournament overview page
+    When the event page for t is visited
+    Then there should be a 'zum Spielplan' button
+    And it should link to tournament Spielplan for t
+
+  Scenario: Spielplan should have several rounds for ko tournament
+    When the Spielplan page for t is visited
+    Then there should be several rounds
+    And there should be exactly 15 matches and 4 rounds
