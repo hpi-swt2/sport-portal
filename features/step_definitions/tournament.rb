b049956c2f39ed65b7b9d4fb764deb3f2963aa62
Erik Brendel <erik.brendel@gmx.de>
2017-12-13 13:54:03 +0100

    More tests. Ref #169

--- features/step_definitions/tournament.rb
+++ features/step_definitions/tournament.rb
@@ -38,5 +38,27 @@ end
 
 And(/^there should be exactly (\d+) matches and (\d+) rounds$/) do |numberOfMatches, numberOfRounds|
   # The minus 1 is needed because the headline of the table is also a tr entry
-  (page.all('table#matches-table tr').count - 1).should == (numberOfMatches + numberOfRounds)
+  expect(all('table#matches-table tr').count - 1).to eq(numberOfMatches + numberOfRounds)
+end
+
+def find_match_on_page(match_num)
+  *unused_rest, match_id = all('table#matches-table form')[match_num - 1][:id].split '_'
+  Match.find match_id.to_i
+end
+
+And(/^the results for match (\d+) \((\d+) : (\d+)\) got inserted$/) do |match_id, points_home, points_away|
+  match = find_match_on_page match_id
+  fill_in "match_#{match.id}_match_points_home", with: points_home
+  fill_in "match_#{match.id}_match_points_away", with: points_away
+  click_on "save_points_#{match.id}"
+end
+
+Then(/^the (home|away) team of match (\d+) comes to the next round$/) do |home_or_away, match_id|
+  match = find_match_on_page match_id
+  team = nil
+  case home_or_away
+  when 'home' then team = match.team_home
+  when 'away' then team = match.team_away
+  end
+  all('a[href="' + team_path(team) + '"]').count == 2
 end
--- features/tournament/spielplan.feature
+++ features/tournament/spielplan.feature
@@ -1,10 +1,10 @@
 Feature: Spielplan for tournament
   As a tournament organizer
-  in order to organise a torunament
+  in order to organise a tournament
   I want to have a complete Spielplan for my ko-tournament event
 
   Background:
-    Given a tournament t with 16 teams
+    Given a tournament t with 23 teams
 
   Scenario: Spielplan should be linked from tournament overview page
     When the event page for t is visited
@@ -14,4 +14,12 @@ Feature: Spielplan for tournament
   Scenario: Spielplan should have several rounds for ko tournament
     When the Spielplan page for t is visited
     Then there should be several rounds
-    And there should be exactly 15 matches and 4 rounds
+    And there should be exactly 22 matches and 5 rounds
+
+  Scenario: Spielplan should update winner teams continuously
+    When the Spielplan page for t is visited
+    And the results for match 1 (30 : 7) got inserted
+    Then the home team of match 1 comes to the next round
+
+    When the results for match 1 (3 : 7) got inserted
+    Then the away team of match 1 comes to the next round
