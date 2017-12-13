b201edac580444da97a56783d2c58f68e8f7dea6
Erik Brendel <erik.brendel@student.hpi.de>
2017-12-13 22:57:57 +0100

    PR cleanup. Ref #169

--- features/step_definitions/tournament.rb
+++ features/step_definitions/tournament.rb
@@ -17,7 +17,6 @@ When(/^the tournament overview page for (.*) is visited$/) do |tournamentName|
   visit event_overview_path (tournament_named tournamentName)
 end
 
-
 When(/^the event page for (.*) is visited$/) do |tournamentName|
   visit event_path (tournament_named tournamentName)
 end
