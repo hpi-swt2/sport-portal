01a572e9013fb854f1082193daa0da917c0c5c2c
Niklas Riekenbrauck <nikriek@gmail.com>
2017-12-17 15:49:31 +0100

    Add to procfile and fix tests

--- spec/tasks/generate_fake_users_spec.rb
+++ spec/tasks/generate_fake_users_spec.rb
@@ -4,14 +4,23 @@ describe 'rake generate_fake_users', type: :task do
   it 'preloads the Rails environment' do
     expect(task.prerequisites).to include 'environment'
   end
-
   it 'runs gracefully with no subscribers' do
     expect { task.execute }.not_to raise_error
   end
 
-  it 'should generate 10 fake users' do
-    task.execute
+  context 'Less than 10 users' do
+    it 'should generate 10 fake users' do
+      expect { task.execute }.to change { User.count }.from(0).to(10)
+    end
+  end
 
-    expect { task.execute }.to change { User.count }.from(0).to(10)
+  context '10 or more users' do
+    before(:each) do
+      10.times { FactoryBot.create(:user) }
+    end
+
+    it 'should not generate new users' do
+      expect { task.execute }.not_to change { User.count }
+    end
   end
 end
