9db03565e12ea9acdb8b12ecda54a13b70669381
fswt <fabian.stolp@student.hpi.uni-potsdam.de>
2017-12-18 09:43:24 +0100

    Changed tests min/max players per team. Ref. #221

--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -22,8 +22,8 @@ RSpec.describe 'events/new', type: :view do
       expect(rendered).to have_field("event_deadline")
       expect(rendered).to have_field("event_startdate")
       expect(rendered).to have_field("event_enddate")
-      expect(rendered).to have_field("event_min_players_per_team")
-      expect(rendered).to have_field("event_max_players_per_team")
+      expect(rendered).to have_field("league_min_players_per_team")
+      expect(rendered).to have_field("league_max_players_per_team")
     end
   end
 
@@ -48,8 +48,8 @@ RSpec.describe 'events/new', type: :view do
       expect(rendered).to have_field("event_deadline")
       expect(rendered).to have_field("event_startdate")
       expect(rendered).to have_field("event_enddate")
-      expect(rendered).to have_field("event_min_players_per_team")
-      expect(rendered).to have_field("event_max_players_per_team")
+      expect(rendered).to have_field("tournament_min_players_per_team")
+      expect(rendered).to have_field("tournament_max_players_per_team")
     end
   end
 end
