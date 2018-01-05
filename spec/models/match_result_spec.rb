f8e9cefd716a25f5f3e9b1c4451ea419e11175fd
Erik Brendel <erik.brendel@gmx.de>
2018-01-05 11:46:49 +0100

    Add MatchResult model-test. Ref #169

--- spec/models/match_result_spec.rb
+++ spec/models/match_result_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe MatchResult, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can have advancing losers' do
+    match_result = FactoryBot.create :match_result, winner_advances: false
+    match = match_result.match
+    match.points_home = 1
+    match.points_away = 2
+    expect(match_result.advancing_participant).to eq(match.loser)
+  end
 end
