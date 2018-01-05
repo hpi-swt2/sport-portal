508d0f81ddc4f63f3d8dfc9ddda8a7b3a34c627c
Erik Brendel <erik.brendel@gmx.de>
2018-01-05 13:08:50 +0100

    Apply feedback. Ref #169

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
   def link_to_participant(participant)
     if participant.present?
-      show_match = participant.is_a?(MatchResult) && !participant.knows_advancing_participant
-      path = show_match ? match_path(participant.match) : team_path(participant.advancing_participant)
-      link_to participant.name, path
+      link_to participant.name, participant_path(participant)
     else
       '---'
     end
+  end
+
+  def participant_path(participant)
+    show_match = participant.is_a?(MatchResult) && !participant.knows_advancing_participant
+    show_match ? match_path(participant.match) : team_path(participant.advancing_participant)
   end
 end
