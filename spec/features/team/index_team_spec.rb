f60e09f24c00395c750f70853e772e3a785b1c4b
Nico Scordialo <nicoscordialo@googlemail.com>
2018-01-06 15:39:59 +0100

    add index test file for teams including test that checks for absence of id col, ref #219

---
+++ spec/features/team/index_team_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Index team page", type: :feature do
+
+  it "should render without an error" do
+    visit teams_path
+  end
+
+  it "should not contain an id column" do
+    @team = FactoryBot.create :team
+    visit teams_path
+    expect(page).to_not have_text "Id"
+  end
+
+end
