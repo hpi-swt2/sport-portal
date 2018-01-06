a33cfba8e1d9429b0096822816675fb817106ab1
Tobias Bredow <tobias.bredow@student.hpi.de>
2018-01-06 17:18:49 +0100

    fixing codeclimate errors. Ref:#128

--- app/models/event.rb
+++ app/models/event.rb
@@ -110,9 +110,9 @@ class Event < ApplicationRecord
   # this is a method that simplifies manual testing, not intended for production use
   # method not used at the moment since it is now testet with joined users
   #def add_test_teams
-    #max_teams.times do |index|
-     # teams << Team.new(name: "Team #{index}", private: false)
-    #end
+  #max_teams.times do |index|
+  #teams << Team.new(name: "Team #{index}", private: false)
+  #end
   #end
 
   def human_player_type
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -311,7 +311,7 @@ RSpec.describe EventsController, type: :controller do
       expect(response).to redirect_to(events_url)
     end
   end
-
+  #this test is only for leagues
   describe "GET League#schedule" do
     it "should generate schedule if not existing" do
       event = League.create! valid_league_attributes
