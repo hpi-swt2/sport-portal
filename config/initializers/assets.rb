e70654cf6369af77451740842de78e5ed17b4313
Jonas Hering <jnshering@gmail.com>
2018-01-08 22:51:47 +0100

    Add images, headers and lorem ipsum text to sections, ref #182

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( sign_in.css )
 Rails.application.config.assets.precompile += %w( welcome.css )
 Rails.application.config.assets.precompile += %w( navbar.css )
+Rails.application.config.assets.precompile += %w( footer.css )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
