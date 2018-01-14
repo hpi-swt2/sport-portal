ca32b6514c19bcdbd9586d6a09cf7af803699ca4
Juliane <julianekleinknecht@gmail.com>
2018-01-14 20:28:01 +0100

    Add backbutton for mobile menu after click on avatar. #183

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
         $('.mobile-avatar').on('click', function() {
             $('#default-mobile-menu').addClass('hidden');
             $('#user-mobile-menu').removeClass('hidden');
+            $('#user-mobile-back').removeClass('hidden');
         });
+    }
+    if($('#user-mobile-back')){
+        $('#user-mobile-back').on('click', function(){
+            $('#default-mobile-menu').removeClass('hidden');
+            $('#user-mobile-menu').addClass('hidden');
+            $('#user-mobile-back').addClass('hidden');
+        })
     }
 })
