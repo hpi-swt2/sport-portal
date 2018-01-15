20369fc7fbd3a1476116badfbb692f9df09e941a
Juliane <julianekleinknecht@gmail.com>
2018-01-15 20:29:30 +0100

    Fix linespacing for codeclimate

--- spec/features/user_avatar_in_navbar_spec.rb
+++ spec/features/user_avatar_in_navbar_spec.rb
@@ -7,7 +7,6 @@ RSpec.feature "User avatar in navbar", type: :feature do
         user = FactoryBot.create :user, :with_avatar
         sign_in user
         visit root_path
-        
         expect(page).to have_css(".navbar img[src='#{user.avatar_url}']")
       end
     end
@@ -17,7 +16,6 @@ RSpec.feature "User avatar in navbar", type: :feature do
         user = FactoryBot.create :user
         sign_in user
         visit root_path
-
         expect(page).to have_css(".navbar img[src='https://owncloud.hpi.de/index.php/s/57REqYtqsah3A7o/download']")
       end
     end
@@ -26,7 +24,6 @@ RSpec.feature "User avatar in navbar", type: :feature do
   context 'user is not signed in' do
     scenario 'User sees no avatar in navbar' do
       visit root_path
-
       expect(page).not_to have_css('.navbar .avatar img')
     end
   end
