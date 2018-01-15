31b1f2feedc3a42e230c026da158b7a834437dfe
Lukas Heilmann <lukas@familieheilmann.de>
2018-01-15 14:33:19 +0100

    Adjust request spec descriptions. Ref #177

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "is forbidden! (now write some real specs)" do
+    it "should be forbidden for non-admins" do
       get users_path
       expect(response).to have_http_status(401)
     end
