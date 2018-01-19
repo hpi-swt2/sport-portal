490973124f2b4890344af99d46d478a11a80a536
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2018-01-19 14:04:43 +0100

    Move picture code to own file and add teams. Ref #370

--- app/assets/javascripts/users/edit_user.js
+++ app/assets/javascripts/users/edit_user.js
@@ -7,15 +7,5 @@ $( document ).on('turbolinks:load', function() {
     $('#user_email').on('input', function () {
         password_block.show();
     });
-    $('#user_avatar').on('change', function (evt) {
-        if (evt.target.files && evt.target.files.length === 1) {
-            var file = evt.target.files[0];
-            var fileReader = new FileReader();
-            fileReader.onload = function () {
-                $('#preview').attr('src', fileReader.result);
-            };
-            fileReader.readAsDataURL(file);
-        }
-    });
 });
 
