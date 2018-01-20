36a2cdafe7b099e038c289ff38f57f4333b8bd8a
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2018-01-20 12:12:52 +0100

    Add some back buttons and excluded routes with cancel buttons. Ref #375

--- features/step_definitions/content.rb
+++ features/step_definitions/content.rb
@@ -58,7 +58,7 @@ end
 
 
 Then(/^there should be a back button on all pages except the start page$/) do
-  excludedrouts = ['/', '/cable', '/tournaments/new', '/leagues/new', '/leagues/1/edit', '/tournaments/1/edit', '/events/1/edit', '/rankinglists/new', '/teams/new', '/teams/1/edit']
+  excludedrouts = ['/', '/cable', '/tournaments/new', '/leagues/new', '/leagues/1/edit', '/tournaments/1/edit', '/events/1/edit', '/rankinglists/new', '/teams/new', '/teams/1/edit', '/matches/1/edit']
   Rails.application.routes.routes.each do |route|
     currentpath = route.path.spec.to_s
     if currentpath.include? '(.:format)'
