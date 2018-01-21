2012edc765422b6089f8ce4764f49259aad92827
Niklas Riekenbrauck <nikriek@gmail.com>
2018-01-21 17:37:31 +0100

    Use dev schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118161255) do
+ActiveRecord::Schema.define(version: 20180107175546) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -28,14 +28,7 @@ ActiveRecord::Schema.define(version: 20180118161255) do
     t.integer "gameday_duration"
     t.integer "owner_id"
     t.float "initial_value"
-    t.integer "matchtype"
-    t.integer "bestof_length", default: 1
-    t.integer "game_winrule"
-    t.integer "points_for_win", default: 3
-    t.integer "points_for_draw", default: 1
-    t.integer "points_for_lose", default: 0
     t.integer "selection_type", default: 0, null: false
-    t.text "image_data"
     t.index ["game_mode"], name: "index_events_on_game_mode"
     t.index ["owner_id"], name: "index_events_on_owner_id"
     t.index ["player_type"], name: "index_events_on_player_type"
@@ -55,15 +48,6 @@ ActiveRecord::Schema.define(version: 20180118161255) do
     t.index ["user_id", "event_id"], name: "index_events_users_on_user_id_and_event_id"
   end
 
-  create_table "game_results", force: :cascade do |t|
-    t.integer "score_home"
-    t.integer "score_away"
-    t.integer "match_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["match_id"], name: "index_game_results_on_match_id"
-  end
-
   create_table "match_results", force: :cascade do |t|
     t.integer "match_id"
     t.boolean "winner_advances"
@@ -74,6 +58,8 @@ ActiveRecord::Schema.define(version: 20180118161255) do
 
   create_table "matches", force: :cascade do |t|
     t.string "place"
+    t.integer "score_home"
+    t.integer "score_away"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "team_home_id"
@@ -85,7 +71,6 @@ ActiveRecord::Schema.define(version: 20180118161255) do
     t.string "team_home_type", default: "Team"
     t.string "team_away_type", default: "Team"
     t.integer "index"
-    t.datetime "start_time"
     t.index ["event_id"], name: "index_matches_on_event_id"
   end
 
