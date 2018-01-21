716ec08c69fa84af110946a204ea2177fc6b32a5
Lariiii <lari-lari@gmx.net>
2018-01-21 15:46:15 +0100

    Add migration for adding and copying a match start_time. Ref. #147

---
+++ db/migrate/20180121144349_copy_startdate_to_starttime.rb
@@ -0,0 +1,8 @@
+class CopyStartdateToStarttime < ActiveRecord::Migration[5.1]
+  def up
+    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id))).to_s.to_datetime)
+  end
+
+  def down
+  end
+end
