67de4210f87386794986d8a11d23e7ba7d07b8e5
Moritz Manner <moritz.manner@student.hpi.de>
2018-01-22 11:09:44 +0100

    Try to make code climate happy. ref #91

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
 
-  private
-  def set_locale
-    I18n.locale = :de
-  end
-
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
       render file: "#{Rails.root}/public/403.html", status: 403, layout: false
@@ -15,8 +10,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  #Redirect to user dashboard after successfull login
+  #Redirect to user dashboard after successful login
   def after_sign_in_path_for(resource)
     dashboard_user_path(resource)
   end
+
+  private
+    def set_locale
+      I18n.locale = :de
+    end
 end
