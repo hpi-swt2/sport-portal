86ca3642293f7270ce7314734d50d0d31d3df905
Lariiii <lari-lari@gmx.net>
2018-01-22 22:33:58 +0100

    Add local variable to migration. Ref. #147

--- db/migrate/20180121144349_copy_startdate_to_starttime.rb
+++ db/migrate/20180121144349_copy_startdate_to_starttime.rb
@@ -1,9 +1,8 @@
 class CopyStartdateToStarttime < ActiveRecord::Migration[5.1]
   def change
-    include ActiveModel::Event
-    define_attribute_methods :startdate
+    start = Event.select(:startdate)
 
-    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id))).first.(:startdate).to_datetime)
+    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id))).first.start.to_datetime)
   end
 
 end
