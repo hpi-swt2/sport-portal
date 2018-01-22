60f7f58ce2cb986fc9ede4d2711a605ca3d44c35
Lariiii <lari-lari@gmx.net>
2018-01-22 22:27:23 +0100

    Add ActiveModel attributes to migration. Ref. #147

--- db/migrate/20180121144349_copy_startdate_to_starttime.rb
+++ db/migrate/20180121144349_copy_startdate_to_starttime.rb
@@ -1,6 +1,9 @@
 class CopyStartdateToStarttime < ActiveRecord::Migration[5.1]
   def change
-    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id)))[0].to_s.to_datetime)
+    include ActiveModel::Event
+    define_attribute_methods :startdate
+
+    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id))).first.(:startdate).to_datetime)
   end
 
 end
