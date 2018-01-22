3fbd8b1813799eafa3a61d30d84d9469474f56d7
Moritz Manner <moritz.manner@student.hpi.de>
2018-01-22 14:28:41 +0100

    Set locale in initializer. ref. #91

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_locale
 
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
@@ -14,9 +13,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     dashboard_user_path(resource)
   end
-
-  private
-    def set_locale
-      I18n.locale = :de
-    end
 end
---
+++ config/initializers/default_locale.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+I18n.locale = :de
