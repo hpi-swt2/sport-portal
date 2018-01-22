56d8a76b50dcfa83f3f8ca341a6860fda05582f8
Lariiii <lari-lari@gmx.net>
2018-01-22 17:18:25 +0100

    Changed migration so it now shows the correct date. Ref. #147

--- db/migrate/20180121144349_copy_startdate_to_starttime.rb
+++ db/migrate/20180121144349_copy_startdate_to_starttime.rb
@@ -1,6 +1,6 @@
 class CopyStartdateToStarttime < ActiveRecord::Migration[5.1]
   def up
-    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id))).to_s.to_datetime)
+    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id)))[0].startdate.to_s.to_datetime)
   end
 
   def down
