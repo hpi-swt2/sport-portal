5927cab705e6bf06e8e59246254c43d8b627922b
Lariiii <lari-lari@gmx.net>
2018-01-22 22:42:03 +0100

    Next try to fix migration. Ref. #147

--- db/migrate/20180121144349_copy_startdate_to_starttime.rb
+++ db/migrate/20180121144349_copy_startdate_to_starttime.rb
@@ -1,6 +1,6 @@
 class CopyStartdateToStarttime < ActiveRecord::Migration[5.1]
   def change
-    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id))).first[:startdate].to_datetime)
+    Match.update_all(:start_time => (Event.select(:startdate).where(Match.select(:event_id)==Event.select(:id)))[0][:startdate].to_datetime)
   end
 
 end
