08a9dd9b3406c04c945cc4b80cfb2d7fc13ea56e
jak-ing <lupin2702@gmail.com>
2018-01-25 20:24:40 +0100

    Add tests for display of application name in navbar and footer partials #364

--- spec/views/application/_navbar.html.erb_spec.rb
+++ spec/views/application/_navbar.html.erb_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "application/_navbar", type: :view do
+  it "shows the application name" do
+    render
+    expect(rendered).to have_link I18n.t('appname'), href: root_path
+  end
+
   it "shows a login button" do
     render
     expect(rendered).to have_link(href: new_user_session_path)
