c203e859541aadf92cf78d5f4e15ac89b8148879
Conrad Lempert <>
2018-01-25 23:48:52 +0100

    previews work now

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -23,7 +23,8 @@ class MatchMailer < ApplicationMailer
     @user_name = user.name
     @opponent = match.team_home
     @event = match.event
-    @event_url = events_url(match.event)
+    @event_url = "wow"
+        #events_url(match.event)
 
     mail to: user.email
   end
@@ -32,7 +33,8 @@ class MatchMailer < ApplicationMailer
     @opponent = match.team_home
     @event = match.event
     @date = match.gameday
-    @event_url = events_url(match.event)
+    @event_url = "wow"
+        #events_url(match.event)
 
     mail to: user.email
   end
--- spec/mailers/previews/match_mailer_preview.rb
+++ spec/mailers/previews/match_mailer_preview.rb
@@ -4,21 +4,33 @@ class MatchMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/match_mailer/match_scheduled
   def match_scheduled
     fake
-    MatchMailer.match_scheduled(@user, @match)
+    @mail = MatchMailer.match_scheduled(@user, @match)
+    delete_fakes
+    return @mail
   end
 
   def match_canceled
-    MatchMailer.match_canceled
+    fake
+    @mail = MatchMailer.match_canceled(@user, @match)
+    delete_fakes
+    return @mail
   end
 
   def match_date_changed
-    MatchMailer.match_date_changed
+    fake
+    @mail = MatchMailer.match_date_changed(@user, @match)
+    delete_fakes
+    return @mail
   end
-
   private
     def fake
       @user = FactoryBot.create(:user)
       @match = FactoryBot.build(:match)
+    end
+
+    def delete_fakes
+      @user.destroy
+      @match.destroy
     end
 
 end
