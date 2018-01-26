8981ad6adcd6cae12e4b424c7a6767dad756d487
Conrad Lempert <>
2018-01-26 10:44:23 +0100

    Add event mails. ref #308

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 54ce6d75a758d7c3ae697b66f6acab27d0d2258f
