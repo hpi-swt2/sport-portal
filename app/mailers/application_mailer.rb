0dadd34f99af4d3b0c62ac371e827895205aea2a
bakoe <ba.koe96@gmail.com>
2018-01-26 17:44:11 +0100

    Add private email_with_name method to ApplicationMailer in order to prettify email recipients. Ref #307

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  private
+    def email_with_name(user)
+      %('#{user.name}' <#{user.email}>)
+    end
 end
