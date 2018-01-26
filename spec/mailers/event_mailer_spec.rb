e76f6edbed8348e8b48617d0079fbcec341ce5d2
Conrad Lempert <>
2018-01-26 14:36:17 +0100

    Add mail tests. ref #308

--- spec/mailers/event_mailer_spec.rb
+++ spec/mailers/event_mailer_spec.rb
@@ -1,18 +1,71 @@
 require "rails_helper"
 
 RSpec.describe EventMailer, type: :mailer do
-  describe "match_scheduled" do
-    #let(:mail) { EventMailer.match_scheduled }
+  describe "event_canceled" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:event1) { FactoryBot.create(:event) }
+    let(:mail) { EventMailer.event_canceled(user, event1) }
+    before(:each) { mail.deliver_now }
 
-    #it "renders the headers" do
-    #  expect(mail.subject).to eq("Match scheduled")
-    #  expect(mail.to).to eq(["to@example.org"])
-    #  expect(mail.from).to eq(["from@example.com"])
-    #end
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
 
-    #it "renders the body" do
-     # expect(mail.body.encoded).to match("Hi")
-    #end
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'assigns user\'s firstname' do
+      expect(mail.body.encoded).to match(user.first_name)
+    end
+
+    it 'assigns event\'s name' do
+      expect(mail.body.encoded).to match(event1.name)
+    end
+  end
+  describe "event_started" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:event1) { FactoryBot.create(:event) }
+    let(:mail) { EventMailer.event_started(user, event1) }
+    before(:each) { mail.deliver_now }
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'assigns user\'s firstname' do
+      expect(mail.body.encoded).to match(user.first_name)
+    end
+
+    it 'assigns event\'s name' do
+      expect(mail.body.encoded).to match(event1.name)
+    end
+  end
+  describe "event_finished" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:event1) { FactoryBot.create(:event) }
+    let(:mail) { EventMailer.event_finished(user, event1) }
+    before(:each) { mail.deliver_now }
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'assigns user\'s firstname' do
+      expect(mail.body.encoded).to match(user.first_name)
+    end
+
+    it 'assigns event\'s name' do
+      expect(mail.body.encoded).to match(event1.name)
+    end
   end
 
 end
--- spec/mailers/match_mailer_spec.rb
+++ spec/mailers/match_mailer_spec.rb
@@ -43,4 +43,103 @@ RSpec.describe MatchMailer, type: :mailer do
       expect(mail.body.encoded).to match(I18n.localize(match1.start_time))
     end
   end
+  describe 'match scheduled' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:match1) { FactoryBot.create(:match) }
+    let(:mail) { MatchMailer.match_scheduled(user, match1) }
+    before(:each) { mail.deliver_now }
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'assigns user\'s firstname' do
+      expect(mail.body.encoded).to match(user.first_name)
+    end
+
+    it 'assigns event\'s name' do
+      expect(mail.body.encoded).to match(match1.event.name)
+    end
+
+    it 'assigns match team home name' do
+      expect(mail.body.encoded).to match(match1.team_home.name)
+    end
+
+    it 'assigns match team away name' do
+      expect(mail.body.encoded).to match(match1.team_away.name)
+    end
+
+    it 'assigns event\'s startdate' do
+      expect(mail.body.encoded).to match(I18n.localize(match1.start_time))
+    end
+  end
+  describe 'match date changed' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:match1) { FactoryBot.create(:match) }
+    let(:mail) { MatchMailer.match_date_changed(user, match1) }
+    before(:each) { mail.deliver_now }
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'assigns user\'s firstname' do
+      expect(mail.body.encoded).to match(user.first_name)
+    end
+
+    it 'assigns event\'s name' do
+      expect(mail.body.encoded).to match(match1.event.name)
+    end
+
+    it 'assigns match team home name' do
+      expect(mail.body.encoded).to match(match1.team_home.name)
+    end
+
+    it 'assigns match team away name' do
+      expect(mail.body.encoded).to match(match1.team_away.name)
+    end
+
+    it 'assigns event\'s startdate' do
+      expect(mail.body.encoded).to match(I18n.localize(match1.start_time))
+    end
+  end
+  describe 'match canceled' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:match1) { FactoryBot.create(:match) }
+    let(:mail) { MatchMailer.match_canceled(user, match1) }
+    before(:each) { mail.deliver_now }
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eq(['from@example.com'])
+    end
+
+    it 'assigns user\'s firstname' do
+      expect(mail.body.encoded).to match(user.first_name)
+    end
+
+    it 'assigns event\'s name' do
+      expect(mail.body.encoded).to match(match1.event.name)
+    end
+
+    it 'assigns match team home name' do
+      expect(mail.body.encoded).to match(match1.team_home.name)
+    end
+
+    it 'assigns match team away name' do
+      expect(mail.body.encoded).to match(match1.team_away.name)
+    end
+
+  end
 end
