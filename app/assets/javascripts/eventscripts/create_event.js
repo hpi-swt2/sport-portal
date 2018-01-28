27969c7484a5a900917afc6c1adc0f7babc91f7c
Tobias Bredow <tobias.bredow@student.hpi.de>
2018-01-28 21:18:34 +0100

    functionality working some test still failing. Ref#296

--- app/assets/javascripts/eventscripts/create_event.js
+++ app/assets/javascripts/eventscripts/create_event.js
@@ -27,6 +27,26 @@ $( document ).on('turbolinks:load', function() {
     {
         var start = $("#event_startdate").val();
         var end = $("#event_enddate").val();
+        var system = $("#event_game_mode").val();
+        var gameday_dur =$("#event_gameday_duration").val();
+        var participants = $("#event_max_teams").val();
+        var rounds = 0;
+        if(start != "" && gameday_dur != "" && system != "")
+        {
+            rounds = calcRounds(system,participants);
+            var number_of_days = rounds * parseInt(gameday_dur);
+            $("#event_duration").val(number_of_days);
+            $("#event_enddate").val(addDays(start,number_of_days));
+
+        }
+        if(start != "" && end != "" && system != "")
+        {
+            rounds = calcRounds(system,participants);
+            var diff = calcDateDiff(start,end);
+            var gameday_duration = Math.ceil(rounds/diff);
+            $("#event_gameday_duration").val(gameday_duration);
+
+        }
         if(start != "" && end != "") {
             diff = calcDateDiff(start,end);
             if (diff <= 0){
@@ -36,12 +56,46 @@ $( document ).on('turbolinks:load', function() {
         }
     }
 
+    function addDays(date, days) {
+        var result = new Date(date);
+        result.setDate(result.getDate() + parseInt(days));
+        var dd = result.getDate();
+        var mm = result.getMonth();
+        var y = result.getFullYear();
+        var formattedDate = y + '-' + mm + '-' + dd;
+        return formattedDate;
+    }
+
     function calcDateDiff(startdatestr, enddatestr)
     {
       var start = new Date(startdatestr);
       var end = new Date(enddatestr);
 
       return  Math.round((end-start)/(1000*60*60*24)) + 1;
+    }
+
+    function calcRounds(system,participants)
+    {
+        var num_part = parseInt(participants);
+        var sys = system;
+        var num_rounds;
+        if(sys == "round_robin" || sys == "two_halfs")
+        {
+            if(num_part % 2 == 0)
+            {
+                num_rounds = num_part - 1;
+            }
+            else
+            {
+                num_rounds = num_part;
+            }
+        }
+        else if (sys == "swiss")
+        {
+            num_rounds = (0.2 * num_part) + (1.4 * num_part);
+        }
+
+        return Math.ceil(num_rounds);
     }
 
     $("#event_duration").on("change", function(){
@@ -81,5 +135,6 @@ $( document ).on('turbolinks:load', function() {
           break;
       }
     }
+
 });
 
