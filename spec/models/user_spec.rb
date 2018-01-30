c06ba633c38ac391c79aab9390ec114f494e8acf
birneamstiel <luckifri@yahoo.de>
2018-01-30 12:36:53 +0100

    Add model comments for new user attributes. Ref. #301

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,24 +2,26 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  first_name             :string
-#  last_name              :string
-#  provider               :string
-#  uid                    :string
-#  admin                  :boolean          default(FALSE)
-#  birthday               :date
-#  telephone_number       :string
-#  telegram_username      :string
-#  favourite_sports       :string
-#  avatar_data            :text
+#  id                          :integer          not null, primary key
+#  email                       :string           default(""), not null
+#  encrypted_password          :string           default(""), not null
+#  reset_password_token        :string
+#  reset_password_sent_at      :datetime
+#  remember_created_at         :datetime
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  first_name                  :string
+#  last_name                   :string
+#  admin                       :boolean          default(FALSE)
+#  birthday                    :date
+#  telephone_number            :string
+#  telegram_username           :string
+#  favourite_sports            :string
+#  provider                    :string
+#  uid                         :string
+#  avatar_data                 :text
+#  team_notifications_enabled  :boolean          default(TRUE)
+#  event_notifications_enabled :boolean          default(TRUE)
 #
 
 require 'rails_helper'
