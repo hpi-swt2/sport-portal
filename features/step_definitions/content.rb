83c44531a56046ac986a6bd629afd8a28d5fef58
Oluwoye <Tim.Cech@student.hpi.uni-potsdam.de>
2018-01-30 09:54:02 +0100

    Added some comments for test. Ref #375

--- features/step_definitions/content.rb
+++ features/step_definitions/content.rb
@@ -58,8 +58,11 @@ end
 
 
 Then(/^there should be a back button on all pages except the start page$/) do
+  # These routes were excluded in accordance with our PO
   excludedrouts = ['/', '/cable', '/tournaments/new', '/leagues/new', '/leagues/1/edit', '/tournaments/1/edit', '/events/1/edit', '/rankinglists/new', '/teams/new', '/teams/1/edit', '/matches/1/edit', '/my/users/sign_in', '/my/users/password/edit', '/my/users/cancel', '/my/users/sign_up', '/users/1/dashboard', '/users/1/edit']
+  # For the first route we really don't know, why this fails. The second route could only be visited as an administrator
   temporarywedontknowwhattodorouts = ['/my/users/password/new', '/users']
+
   Rails.application.routes.routes.each do |route|
     currentpath = route.path.spec.to_s
     if currentpath.include? '(.:format)'
@@ -68,9 +71,8 @@ Then(/^there should be a back button on all pages except the start page$/) do
     if currentpath.include? ':id'
       currentpath[':id'] = '1'
     end
-    if currentpath.include? '/events/'
-      currentpath['/events/'] = '/tournaments/'
-    end
+
+    # There are a lot of routs in Rails, that can't be visited in general
     begin
       visit currentpath
     rescue Exception
