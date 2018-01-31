04e700acde9240fc8ae60eb0c6b8e563a6c3b8d7
Niklas Riekenbrauck <nikriek@gmail.com>
2018-01-31 20:27:21 +0100

    Fix rubocop

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -79,7 +79,7 @@ class Ability
 
 
     def can_modify_match(user)
-      user_team_ids = user.team_users.map {|team_user| team_user.id}
+      user_team_ids = user.team_users.map { |team_user| team_user.id }
       can :modify, Match do |match|
         (user_team_ids & [match.team_home.id, match.team_away.id]).any?
       end
