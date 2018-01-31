b060590642a2c2837c531f5fa63255c243296ab8
Marius Danner <marius.danner@student.hpi.de>
2018-01-31 13:57:23 +0100

    Fix indentation. #379

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -101,7 +101,7 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
     def set_event
       @event = Event.find(params[:id])
     end
