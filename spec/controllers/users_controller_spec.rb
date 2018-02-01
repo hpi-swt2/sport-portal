1352f0b8c0ef1b2777cae6702c8e381eb79e64cd
birneamstiel <luckifri@yahoo.de>
2018-02-01 19:15:59 +0100

    Test correct behavior of added update_notifications controller action. Ref. #489

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -334,4 +334,13 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe 'PATCH #update_notifications' do
+    it 'should update notifications settings successfully' do
+      sign_in @user
+      patch :update_notifications, params: { id: @user.to_param, user: { event_notifications_enabled: false, team_notifications_enabled: false } }
+      @user.reload
+      expect(@user.event_notifications_enabled?).to eq(false)
+      expect(@user.team_notifications_enabled).to eq(false)
+    end
+  end
 end
