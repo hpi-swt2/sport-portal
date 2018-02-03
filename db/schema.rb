376c24dfdd808d264afc5f7124dc6130c4604a31
Erik Brendel <erik.brendel@student.hpi.de>
2018-02-03 18:35:46 +0100

    Addd contact_information to schema.rb. Ref #486

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202093132) do
+ActiveRecord::Schema.define(version: 20180203172805) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -151,15 +151,16 @@ ActiveRecord::Schema.define(version: 20180202093132) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin", default: false
-    t.string "provider"
-    t.string "uid"
     t.date "birthday"
     t.string "telephone_number"
     t.string "telegram_username"
     t.string "favourite_sports"
+    t.string "provider"
+    t.string "uid"
     t.text "avatar_data"
     t.boolean "team_notifications_enabled", default: true
     t.boolean "event_notifications_enabled", default: true
+    t.string "contact_information"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
