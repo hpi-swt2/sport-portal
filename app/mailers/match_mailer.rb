bb74a78a955f6df295eb9a96c3c280354930b0c2
Lukas Heilmann <lukas@familieheilmann.de>
2018-02-05 22:00:55 +0100

    Adjust all match mail templates after redefining acceptance criteria. Ref #513

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -4,14 +4,10 @@ class MatchMailer < ApplicationMailer
     @user = user
     @match = match
     @opponent_team = (@match.team_home.has_member? @user) ? @match.team_away : @match.team_home
-    @place = @match.place.blank? ? '' : (@place.prepend t('match_mailer.in'))
 
     subject = t("match_mailer.#{template}.subject",
                 team_name: @opponent_team.name,
-                event_name: @match.event.name,
-                start_date: @match.start_time.to_date,
-                start_time: @match.start_time.to_s(:time),
-                place: @place)
+                event_name: @match.event.name)
 
     mail(to: user.email,
          subject: subject) do |format|
