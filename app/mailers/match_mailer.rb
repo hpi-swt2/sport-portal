06640517c4ec15217cd248b3a4a523d581c4df98
Lukas Heilmann <lukas@familieheilmann.de>
2018-02-05 21:16:03 +0100

    Remove TODO's, add calculation of opponent_team, implement check on place of match. Ref #494

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -3,12 +3,22 @@ class MatchMailer < ApplicationMailer
   def send_mail(user, match, template)
     @user = user
     @match = match
+    @opponent_team = (@match.team_home.has_member? @user) ? @match.team_away : @match.team_home
+    @place = @match.place.blank? ? '' : (@place.prepend t('match_mailer.in'))
+
+    subject = t("match_mailer.#{template}.subject",
+                team_name: @opponent_team.name,
+                event_name: @match.event.name,
+                start_date: @match.start_time.to_date,
+                start_time: @match.start_time.to_s(:time),
+                place: @place)
 
     mail(to: user.email,
-         subject: t("match_mailer.#{template}.subject")) do |format|
+         subject: subject) do |format|
       format.html { render template }
       format.text { render template }
     end
+
     prevent_delivery_to_event_unsubscribed_users(user)
   end
 end
