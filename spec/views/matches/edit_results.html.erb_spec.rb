010ad000728e19fb08249072a6902fe756f608fc
Niklas Riekenbrauck <nikriek@gmail.com>
2018-02-06 19:25:59 +0100

    fix some tests

--- spec/views/matches/edit_results.html.erb_spec.rb
+++ spec/views/matches/edit_results.html.erb_spec.rb
@@ -1,25 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "matches/edit_results", type: :view do
-  before(:each) do
-    @match = assign(:match, FactoryBot.create(:match, :with_results))
+  let(:match) { FactoryBot.create(:match, :with_results) }
 
+  before(:each) do
+    @match = assign(:match, match)
     @ability = Object.new
     @ability.extend(CanCan::Ability)
-    @match = assign(:current_ability, @ability)
-    render
+    controller.stub(:current_ability) { @ability }
   end
 
   it "renders a input form" do
-    expect(rendered).to have_css("form[action='#{match_path(@match)}'][method='post']", count: 2)
+    render
+
+    expect(rendered).to have_css("form[action='#{update_results_match_path(@match)}'][method='post']", count: 2)
   end
 
   it "has score home and score away input fields" do
+    render
+
     expect(rendered).to have_xpath("//div[@class='result']//input[@type='text' and @value='#{@match.game_results.first.score_home}']")
     expect(rendered).to have_xpath("//div[@class='result']//input[@type='text' and @value='#{@match.game_results.first.score_away}']")
   end
 
   it "has buttons to add and remove results" do
+    render
+
     expect(rendered).to have_xpath("//a[@href='#' and contains(@class,'remove') and contains(@class,'btn')]")
     expect(rendered).to have_xpath("//a[@href='#' and contains(@class,'add') and contains(@class,'btn')]")
 
@@ -31,22 +37,21 @@ RSpec.describe "matches/edit_results", type: :view do
   context 'can confirm result' do
     before(:each) do
       @ability.can :confirm, GameResult
-      render
     end
 
     it 'should have a confirm button' do
-      expect(rendered).to have_xpath("//a[@href=#{confirm_game_result_match(match, match.game_results.first)}]")
+      render
+      expect(rendered).to have_xpath("//a[@href='#{confirm_game_result_match_path(match, match.game_results.first)}']")
     end
   end
 
   context 'cannot confirm result' do
     before(:each) do
-      @ability.can :confirm, GameResult
-      render
+      @ability.cannot :confirm, GameResult
     end
 
     it 'should not have a confirm button' do
-      expect(rendered).not_to have_xpath("//a[@href=#{confirm_game_result_match(match, match.game_results.first)}]")
+      expect(rendered).not_to have_xpath("//a[@href='#{confirm_game_result_match_path(match, match.game_results.first)}']")
     end
   end
 end
